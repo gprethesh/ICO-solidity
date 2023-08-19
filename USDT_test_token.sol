6ea76a443498c1026fe08847c1bf627c8b7b9e28
George <116725807+gprethesh@users.noreply.github.com>
2023-08-19 13:18:05 +0530

    Update USDT_test_token.sol

--- USDT_test_token.sol
+++ USDT_test_token.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract USDT is ERC20 {
+    constructor(uint256 initialSupply) ERC20("USDT", "USDT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function transferHumanReadable(address recipient, uint256 amount) public returns (bool) {
+        uint256 amountInBaseUnits = amount * (10 ** decimals());
+        return super.transfer(recipient, amountInBaseUnits);
+    }
+}
 
